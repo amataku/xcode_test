dc64ea62f4d9f059ed6e774574c1d054818f5831
amataku <amataku13@gmail.com>
2018-04-13 18:39:27 +0900

    add action

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func tapHandle(_ sender: Any) {
+        textField.text = "変更しました";
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
